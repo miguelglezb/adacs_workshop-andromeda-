56360541d525c959832ce17406d78ec05bb7c7fa
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-03-20 14:38:51 +1100

    Fixing syntax with pylint

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,38 +1,38 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+"""
+Determine Andromeda location in ra/dec degrees
+"""
 
-
-# Determine Andromeda location in ra/dec degrees
+import random
+from math import cos,pi
 
 # from wikipedia
-ra = '00:42:44.3'
-dec = '41:16:09'
+RA = '00:42:44.3'
+# ^- Constant name "ra" doesn't conform to UPPER_CASE naming style (invalid-name)
+DEC = '41:16:09'
+# ^- Constant name "dec" doesn't conform to UPPER_CASE naming style (invalid-name)
 
-# convert to decimal degrees
-from math import *
-
-d, m, s = dec.split(':')
+d, m, s = DEC.split(':')
 dec = int(d)+int(m)/60+float(s)/3600
 
-h, m, s = ra.split(':')
+h, m, s = RA.split(':')
 ra = 15*(int(h)+int(m)/60+float(s)/3600)
 ra = ra/cos(dec*pi/180)
 
-nsrc = 1_000_000
+NSRC = 1_000_000
+# ^- Constant name "nsrc" doesn't conform to UPPER_CASE naming style (invalid-name)
 
 # make 1000 stars within 1 degree of Andromeda
-from random import *
 ras = []
 decs = []
-for i in range(nsrc):
-    ras.append(ra + uniform(-1,1))
-    decs.append(dec + uniform(-1,1))
+for i in range(NSRC):
+    ras.append(ra + random.uniform(-1,1))
+    decs.append(dec + random.uniform(-1,1))
 
 
 # now write these to a csv file for use by my other program
-f = open('catalog.csv','w')
-print("id,ra,dec", file=f)
-for i in range(nsrc):
-    print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
 
-    
+with open ('catalog.csv','w', encoding="utf-8") as f:
+
+    print("id,ra,dec", file=f)
+    for i in range(NSRC):
+        print(f"{0:07d}, {1:12f}, {2:12f}", file=f)
