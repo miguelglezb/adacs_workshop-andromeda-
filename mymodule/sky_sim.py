26472a0bf21ded2bd61ed522cc9a6b2ee9bed880
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-03-21 10:59:58 +1100

    Add documentation for get_radec

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -12,22 +12,23 @@ NSRC = 1_000_000
 
 def get_radec():
     """
-    Determine the Andromeda location
+    Determine the Andromeda location. It uses the coordinates RA and DEC from wikipedia and transforms them into hours.
+    This function returns a tuple in the form (ra, dec)
     """
     RA = '00:42:44.3'
     DEC = '41:16:09'
 
     d, m, s = DEC.split(':')
-    dec = int(d)+int(m)/60+float(s)/3600
+    dec = int(d)+int(m)/60+float(s)/3600 #Converts dec into hours
 
     h, m, s = RA.split(':')
-    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = 15*(int(h)+int(m)/60+float(s)/3600) #Converts ra into hours
     ra = ra/cos(dec*pi/180)
     return (ra, dec)
 
 def make_stars(ra,dec,num_stars):
     """
-    Make 1000 within 1 degree of Andromeda
+    Make num_stars (number of stars) within 1 degree of Andromeda
     """
     ras = []
     decs = []
