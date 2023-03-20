6cfea6d860753f2a7c46ca5141a73f841c22811a
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-03-20 15:19:16 +1100

    Adding __main() in script

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,38 +1,51 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
 """
-Determine Andromeda location in ra/dec degrees
-"""
+A script to simulate a population of stars around the Andromeda galaxy """
 
 import random
 from math import cos,pi
 
-# from wikipedia
-RA = '00:42:44.3'
-# ^- Constant name "ra" doesn't conform to UPPER_CASE naming style (invalid-name)
-DEC = '41:16:09'
-# ^- Constant name "dec" doesn't conform to UPPER_CASE naming style (invalid-name)
-
-d, m, s = DEC.split(':')
-dec = int(d)+int(m)/60+float(s)/3600
-
-h, m, s = RA.split(':')
-ra = 15*(int(h)+int(m)/60+float(s)/3600)
-ra = ra/cos(dec*pi/180)
-
 NSRC = 1_000_000
-# ^- Constant name "nsrc" doesn't conform to UPPER_CASE naming style (invalid-name)
-
-# make 1000 stars within 1 degree of Andromeda
-ras = []
-decs = []
-for i in range(NSRC):
-    ras.append(ra + random.uniform(-1,1))
-    decs.append(dec + random.uniform(-1,1))
 
 
-# now write these to a csv file for use by my other program
+def get_radec():
+    """
+    Determine the Andromeda location
+    """
+    RA = '00:42:44.3'
+    DEC = '41:16:09'
 
-with open ('catalog.csv','w', encoding="utf-8") as f:
+    d, m, s = DEC.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
 
-    print("id,ra,dec", file=f)
+    h, m, s = RA.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/cos(dec*pi/180)
+    return (ra, dec)
+
+def make_stars(ra,dec,num_stars):
+    """
+    Make 1000 within 1 degree of Andromeda
+    """
+    ras = []
+    decs = []
     for i in range(NSRC):
-        print(f"{0:07d}, {1:12f}, {2:12f}", file=f)
+        ras.append(ra + random.uniform(-1,1))
+        decs.append(dec + random.uniform(-1,1))
+    return(ras, decs)
+
+
+
+def main():
+    ra,dec = get_radec()
+    ras,decs = make_stars(ra,dec,NSRC)
+    with open ('catalog.csv','w', encoding="utf-8") as f:
+        print("id,ra,dec", file=f)
+        for i in range(NSRC):
+            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+
+
+if __name__  == '__main__':
+    main()
