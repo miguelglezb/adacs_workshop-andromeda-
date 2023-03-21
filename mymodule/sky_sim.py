6082f284619c9f07911327aed08b6072b0502345
Miguel Gonzalez-Bolivar <miguelgb@astro.unam.mx>
2023-03-21 11:49:41 +1100

    Add CLI (command line interface) for quick run

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -6,7 +6,7 @@ A script to simulate a population of stars around the Andromeda galaxy """
 
 import random
 from math import cos,pi
-
+import argparse
 NSRC = 1_000_000
 
 
@@ -46,6 +46,44 @@ def main():
         print("id,ra,dec", file=f)
         for i in range(NSRC):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+
+
+
+def skysim_parser():
+    """
+    Configure the argparse for skysim
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        The parser for skysim.
+    """
+    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-')
+    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
+                        help="Central ra (degrees) for the simulation location")
+    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
+                        help="Central dec (degrees) for the simulation location")
+    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
+                        help='destination for the output catalog')
+    return parser
+
+if __name__ == "__main__":
+    parser = skysim_parser()
+    options = parser.parse_args()
+    # if ra/dec are not supplied the use a default value
+    if None in [options.ra, options.dec]:
+        ra, dec = get_radec()
+    else:
+        ra = options.ra
+        dec = options.dec
+    
+    ras, decs = make_stars(ra,dec,NSRC)
+    # now write these to a csv file for use by my other program
+    with open(options.out,'w') as f:
+        print("id,ra,dec", file=f)
+        for i in range(NSRC):
+            print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+    print(f"Wrote {options.out}")
 
 
 if __name__  == '__main__':
